98ab497fe39886a865ee4bff9421676339a3bfb1
toswm128 <chominsu10@gmail.com>
2021-03-16 20:10:16 +0900

    [ADD] set up (+counter)

--- mobx/src/App.js
+++ mobx/src/App.js
@@ -1,9 +1,13 @@
-
+import React,{useState} from "react";
 
 function App() {
+  const [count, countItem] = useState(0)
   return (
     <div className="App">
-      a
+      <h1>App.js</h1>
+      <p>{count}</p>
+      <button onClick={()=>{countItem(count+1)}}>+</button>
+      <button onClick={()=>{countItem(count-1)}}>-</button>
     </div>
   );
 }
